855d2fc577dddadd589f841a6abacd761e0eb4c4
Peter S <78850034+peters-axon@users.noreply.github.com>
2025-08-06 08:37:27 +0200

    Release/12.0 (#58)

--- kafka-connector/src/com/axonivy/connector/kafka/KafkaService.java
+++ kafka-connector/src/com/axonivy/connector/kafka/KafkaService.java
@@ -1,21 +1,27 @@
 package com.axonivy.connector.kafka;
 
-import java.util.Comparator;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Properties;
-import java.util.Set;
+import java.util.concurrent.Future;
 import java.util.function.Function;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
+import org.apache.avro.generic.GenericData;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.utils.Utils;
 
-import ch.ivyteam.ivy.bpm.error.BpmError;
+import ch.ivyteam.ivy.application.IProcessModelVersion;
 import ch.ivyteam.ivy.environment.Ivy;
-import ch.ivyteam.ivy.vars.Variable;
+import ch.ivyteam.ivy.project.IIvyProject;
+import ch.ivyteam.ivy.restricted.IvyThreadLocalNameConstants;
+import ch.ivyteam.util.threadcontext.IvyThreadContext;
 
 /**
  * Functions to support working with Apache Kafka.
@@ -25,13 +31,8 @@ public class KafkaService {
 	private static final String POLL_TIMEOUT_MS = "pollTimeoutMs";
 	private static final String TOPIC_CONSUMER_SUPPLIER = "topicConsumerSupplier";
 	private static KafkaService INSTANCE = new KafkaService();
-	private static final String KAFKA_GLOBAL_VARIABLE = "kafka-connector";
 
 	private KafkaService() {
-	}
-
-	public String getKafkaGlobalVariableName() {
-		return KAFKA_GLOBAL_VARIABLE;
 	}
 
 	/**
@@ -44,53 +45,133 @@ public class KafkaService {
 	}
 
 	/**
-	 * Create a {@link KafkaProducer} configured by given properties name.
+	 * Get a {@link Producer} configured by configuration name.
 	 * 
-	 * @param <K>
-	 * @param <V>
-	 * @param properties
-	 * @return
-	 */
-	public <K, V> KafkaProducer<K, V> createProducer(Properties properties) {
-		return executeWithKafkaClassLoader(() -> new KafkaProducer<>(properties));
-	}
-
-	/**
-	 * Create a {@link KafkaProducer} configured by configuration name.
+	 * Re-uses cached producer if available.
 	 * 
 	 * @param <K>
 	 * @param <V>
 	 * @param configurationName
 	 * @return
 	 */
-	public <K, V> KafkaProducer<K, V> createProducer(String configurationName) {
-		return createProducer(getConfigurationProperties(configurationName));
+	@SuppressWarnings("unchecked")
+	public <K, V> Producer<K, V> producer(String configurationName) {
+		var cached = KafkaConfiguration.get(configurationName);
+		if(!cached.isProducerValid()) {
+			synchronized(this) {
+				if(!cached.isProducerValid()) {
+					if(cached.getProducer() != null) {
+						Ivy.log().info("Closing existing {0} for configuration ''{1}''", ProjectAwareKafkaProducer.class.getSimpleName(), configurationName);
+						cached.getProducer().close();
+						cached.setProducer(null);
+					}
+					Ivy.log().info("Creating a new {0} for configuration ''{1}''", ProjectAwareKafkaProducer.class.getSimpleName(), configurationName);
+					cached.setProducer(producer(cached.getProperties()));
+					cached.setProducerValid(true);
+				}
+			}
+		}
+		return (Producer<K, V>) cached.getProducer();
 	}
 
 	/**
-	 * Create a {@link KafkaConsumer} configured by given properties.
+	 * Create a {@link Consumer} configured by configuration name.
 	 * 
-	 * @param <K>
-	 * @param <V>
-	 * @param properties
-	 * @return
-	 */
-	public <K, V> KafkaConsumer<K, V> createConsumer(Properties properties) {
-		return executeWithKafkaClassLoader(() -> new KafkaConsumer<>(properties));
-	}
-
-	/**
-	 * Create a {@link KafkaConsumer} configured by configuration name.
+	 * Consumers are not thread-safe, so always create a new consumer.
 	 * 
 	 * @param <K>
 	 * @param <V>
 	 * @param configurationName
 	 * @return
 	 */
-	public <K, V> KafkaConsumer<K, V> createConsumer(String configurationName) {
-		return createConsumer(getConfigurationProperties(configurationName));
+	public <K, V> Consumer<K, V> consumer(String configurationName) {
+		return consumer(KafkaConfiguration.get(configurationName).getProperties());
 	}
 
+	/**
+	 * Convenience method to send messages from Java code.
+	 * 
+	 * <p>
+	 * Sending from Java code is recommended, when a Schema Registry is used and objects
+	 * should be sent and received (serialized and deserialized automatically), i.e.
+	 * you have set the variable </code>specific.avro.deserializing: true</code>.
+	 * Sending cannot be done with the connector sub-process in this case because
+	 * it does not have access to classes of your project and deserialization will fail
+	 * with a "class not found" error.
+	 * </p>
+	 * 
+	 * <p>
+	 * Note, that you can still use the connector sub-process for sending if you set
+	 * </code>specific.avro.deserializing: false</code>. In this case, objects received
+	 * will not be deserialized directly into your objects, but rather a {@link GenericData.Record}
+	 * will be returned. This might be enough, if only a few attributes of the object
+	 * are needed.
+	 * </p>
+	 * 
+	 * @param <K>
+	 * @param <V>
+	 * @param configurationName
+	 * @param topic
+	 * @param key
+	 * @param value
+	 * @param callback can be <code>null</code>
+	 * @return
+	 */
+	public <K, V> Future<RecordMetadata> send(String configurationName, String topic, K key, V value, Callback callback) {
+		return producer(configurationName).send(new ProducerRecord<>(topic, key, value), callback);
+	}
+
+	/**
+	 * Create a {@link Producer} configured by given properties name.
+	 * 
+	 * @param <K>
+	 * @param <V>
+	 * @param properties
+	 * @return
+	 */
+	public <K, V> Producer<K, V> producer(Properties properties) {
+		Ivy.log().debug("Creating producer with properties: {0}", properties);
+		var producer = executeWithKafkaClassLoader(() -> new KafkaProducer<K, V>(properties));
+		return new ProjectAwareKafkaProducer<K, V>(producer);
+	}
+
+	/**
+	 * Create a {@link Consumer} configured by given properties.
+	 * 
+	 * @param <K>
+	 * @param <V>
+	 * @param properties
+	 * @return
+	 */
+	public <K, V> Consumer<K, V> consumer(Properties properties) {
+		Ivy.log().debug("Creating consumer with properties: {0}", properties);
+		var consumer = executeWithKafkaClassLoader(() -> new KafkaConsumer<K, V>(properties));
+		return new ProjectAwareKafkaConsumer<K, V>(consumer);
+	}
+
+	/**
+	 * Close a producer.
+	 * 
+	 * @param configurationName
+	 */
+	public synchronized void closeProducer(String configurationName) {
+		var cached = KafkaConfiguration.get(configurationName);
+		if(cached.isProducerValid()) {
+			cached.getProducer().close();
+			cached.setProducer(null);
+			cached.setProducerValid(false);
+		}
+	}
+
+	public void closeAllProducers() {
+		for (var configurationName : KafkaConfiguration.getAllCachedConfigurations()) {
+			try {
+				closeProducer(configurationName);
+			} catch (Exception e) {
+				Ivy.log().error("Exception closing producer {0}", configurationName);
+			}
+		}
+	}
 
 	/**
 	 * Execute a function with a different {@link ClassLoader}.
@@ -125,13 +206,26 @@ public class KafkaService {
 	}
 
 	/**
+	 * Execute a function with the current project's (i.e. the project where this request was started) {@link ClassLoader}.
+	 * 
+	 * @param <T>
+	 * @param supplier
+	 * @return
+	 */
+	public <T> T executeWithProjectClassLoader(Supplier<T> supplier) {
+		return executeWithClassLoader(
+				IIvyProject.of(IProcessModelVersion.current())
+				.getProjectClassLoader(), supplier);
+	}
+
+	/**
 	 * Get the configured worker pool size.
 	 * 
 	 * @return
 	 */
 	public int getWorkerPoolSize() {
 		int defaultValue = 10;
-		return getVar(WORKER_POOL_SIZE, v -> StringUtils.isNotBlank(v) ? Integer.valueOf(v) : defaultValue, defaultValue);
+		return getKafkaVar(WORKER_POOL_SIZE, v -> StringUtils.isNotBlank(v) ? Integer.valueOf(v) : defaultValue, defaultValue);
 	}
 
 	/**
@@ -141,7 +235,7 @@ public class KafkaService {
 	 */
 	public long getPollTimeoutMs() {
 		long defaultValue = 60000L;
-		return getVar(POLL_TIMEOUT_MS, v -> StringUtils.isNotBlank(v) ? Long.valueOf(v) : defaultValue, defaultValue);
+		return getKafkaVar(POLL_TIMEOUT_MS, v -> StringUtils.isNotBlank(v) ? Long.valueOf(v) : defaultValue, defaultValue);
 	}
 
 	/**
@@ -150,40 +244,12 @@ public class KafkaService {
 	 * @return
 	 */
 	public String getTopicConsumerSupplier() {
-		return getVar(TOPIC_CONSUMER_SUPPLIER, v -> ( StringUtils.isNotBlank(v) ? v.trim() : null), (String)null);
+		return getKafkaVar(TOPIC_CONSUMER_SUPPLIER, v -> ( StringUtils.isNotBlank(v) ? v.trim() : null), (String)null);
 	}
 
-	/**
-	 * Return the configuration properties of a specific Kafka configuration stored in global variables.
-	 * 
-	 * @param configurationName
-	 * @return
-	 */
-	public Properties getConfigurationProperties(String configurationName) {
-		Properties properties = new Properties();
-		Set<String> seen = new HashSet<>();
-
-		mergeProperties(configurationName, seen, properties);
-
-		return properties;
-	}
-
-	/**
-	 * Convert {@link Properties} to {@link String}.
-	 * 
-	 * @param properties
-	 * @return
-	 */
-	public String getPropertiesString(Properties properties) {
-		return properties.entrySet().stream()
-				.sorted(Comparator.comparing(e -> e.getKey().toString()))
-				.map(e -> String.format("%s: %s", e.getKey(), e.getValue()))
-				.collect(Collectors.joining("\n"));
-	}
-
-	protected <T> T getVar(String varName, Function<String, T> converter, T defaultValue) {
+	protected <T> T getKafkaVar(String varName, Function<String, T> converter, T defaultValue) {
 		T returnValue = defaultValue;
-		String value = Ivy.var().get(varName);
+		var value = Ivy.var().get("%s.%s".formatted(KafkaConfiguration.getKafkaGlobalVariable(), varName));
 		try {
 			returnValue = converter.apply(value);
 		}
@@ -195,40 +261,55 @@ public class KafkaService {
 		return returnValue;
 	}
 
+
 	/**
-	 * Read properties from global variables.
+	 * Create a callback for use within most parts of the Ivy environment.
 	 * 
-	 * If a configuration contains a value for inherited,
-	 * then the inherited configuration will be read first.
+	 * <p>
+	 * Note, that some Ivy objects might not be valid in the callback (e.g. request and response).
+	 * <h2>Examples</h2>
+	 * </p>
 	 * 
-	 * @param configurationName
-	 * @param seen
-	 * @param properties
+	 * <code>
+	 * <pre>
+	 * producer.send(record, ivyCallback(new MyIvyCallback());
+	 * </pre>
+	 * </code>
+	 * or
+	 * <code>
+	 * <pre>
+	 * producer.send(record, ivyCallback((metadata, exception) -> Ivy.wf().signals().send("kafka:demo:signal")));
+	 * </pre>
+	 * </code>
+	 * 
+	 * @param callback
+	 * @return
 	 */
-	protected void mergeProperties(String configurationName, Set<String> seen, Properties properties) {
-		if(!seen.add(configurationName)) {
-			throw BpmError.create("kafka:connector:configloop").withMessage("Found configuration loop with already seen configuration '" + configurationName + "'.").build();
+	public IvyCallback ivyCallback(Callback callback) {
+		return new IvyCallback(callback);
+	}
+
+	private class IvyCallback implements Callback {
+		private Object memento;
+		private Callback callback;
+
+		@SuppressWarnings("restriction")
+		protected IvyCallback(Callback callback) {
+			// Save most of the Ivy context but not the servlet request. It will be invalid and every access will throw an
+			// IllegalStateException, e.g. Ivy.log() which adds data from the servlet request to the log context.
+			this.memento = IvyThreadContext.saveToMemento(List.of(IvyThreadLocalNameConstants.SERVLET_REQUEST));
+			this.callback = callback;
 		}
-		String whichAbs = KAFKA_GLOBAL_VARIABLE + "." + configurationName + ".";
 
-		Properties inheritedProperties = new Properties();
-		Properties newProperties = new Properties();
-
-		for (Variable v : Ivy.var().all()) {
-			String name = v.name();
-			if(name.startsWith(whichAbs)) {
-				name = name.substring(whichAbs.length());
-				String value = v.value();
-				if(name.equals("inherit")) {
-					mergeProperties(value, seen, inheritedProperties);
-				}
-				else {
-					newProperties.put(name, value);
-				}
+		@Override
+		public void onCompletion(RecordMetadata metadata, Exception exception) {
+			try {
+				IvyThreadContext.restoreFromMemento(memento);
+				callback.onCompletion(metadata, exception);
+			}
+			finally {
+				IvyThreadContext.remove();
 			}
 		}
-
-		properties.putAll(inheritedProperties);
-		properties.putAll(newProperties);
 	}
 }
---
+++ kafka-connector/src/com/axonivy/connector/kafka/ProjectAwareKafkaProducer.java
@@ -0,0 +1,133 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.axonivy.connector.kafka;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Future;
+
+import org.apache.kafka.clients.consumer.ConsumerGroupMetadata;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.Metric;
+import org.apache.kafka.common.MetricName;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.Uuid;
+import org.apache.kafka.common.errors.ProducerFencedException;
+
+
+public class ProjectAwareKafkaProducer<K, V> implements Producer<K, V> {
+	protected KafkaProducer<K, V> delegate;
+
+	public ProjectAwareKafkaProducer(KafkaProducer<K, V> delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	public int hashCode() {
+		return delegate.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return delegate.equals(obj);
+	}
+
+	@Override
+	public String toString() {
+		return delegate.toString();
+	}
+
+	@Override
+	public void initTransactions() {
+		delegate.initTransactions();
+	}
+
+	@Override
+	public void beginTransaction() throws ProducerFencedException {
+		delegate.beginTransaction();
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void sendOffsetsToTransaction(Map<TopicPartition, OffsetAndMetadata> offsets, String consumerGroupId)
+			throws ProducerFencedException {
+		delegate.sendOffsetsToTransaction(offsets, consumerGroupId);
+	}
+
+	@Override
+	public void sendOffsetsToTransaction(Map<TopicPartition, OffsetAndMetadata> offsets,
+			ConsumerGroupMetadata groupMetadata) throws ProducerFencedException {
+		delegate.sendOffsetsToTransaction(offsets, groupMetadata);
+	}
+
+	@Override
+	public void commitTransaction() throws ProducerFencedException {
+		delegate.commitTransaction();
+	}
+
+	@Override
+	public void abortTransaction() throws ProducerFencedException {
+		delegate.abortTransaction();
+	}
+
+	@Override
+	public Future<RecordMetadata> send(ProducerRecord<K, V> record) {
+		return KafkaService.get().executeWithProjectClassLoader(() -> delegate.send(record));
+	}
+
+	@Override
+	public Future<RecordMetadata> send(ProducerRecord<K, V> record, Callback callback) {
+		return KafkaService.get().executeWithProjectClassLoader(() -> delegate.send(record, callback));
+	}
+
+	@Override
+	public void flush() {
+		delegate.flush();
+	}
+
+	@Override
+	public List<PartitionInfo> partitionsFor(String topic) {
+		return delegate.partitionsFor(topic);
+	}
+
+	@Override
+	public Map<MetricName, ? extends Metric> metrics() {
+		return delegate.metrics();
+	}
+
+	@Override
+	public Uuid clientInstanceId(Duration timeout) {
+		return delegate.clientInstanceId(timeout);
+	}
+
+	@Override
+	public void close() {
+		delegate.close();
+	}
+
+	@Override
+	public void close(Duration timeout) {
+		delegate.close(timeout);
+	}
+}
